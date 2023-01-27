8d56ab82410464b74eb4b507cac9a81198682dc6
Saikiran8844 <nannapanenisaikiran8844@gmail.com>
2023-01-27 09:39:15 +0530

    first commit

---
+++ src/main/java/com/hicounselor/BookReview/BookReviewApplication.java
@@ -0,0 +1,14 @@
+package com.hicounselor.BookReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReviewApplication.class, args);
+		System.out.println();
+	}
+
+}
